88309adea10e74eac454f52a3e90637cacbfb5ab
zhouyan12355 <138267115+zhouyan12355@users.noreply.github.com>
2024-07-03 16:57:13 +0800

    2

---
+++ backend/unet/predict.py
@@ -0,0 +1,139 @@
+import os
+import random
+from glob import glob
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+
+# 定义 Dice 系数
+def dice_coef(y_true, y_pred, smooth=100):
+    y_true_flatten = K.flatten(y_true)
+    y_pred_flatten = K.flatten(y_pred)
+
+    intersection = K.sum(y_true_flatten * y_pred_flatten)
+    union = K.sum(y_true_flatten) + K.sum(y_pred_flatten)
+    return (2 * intersection + smooth) / (union + smooth)
+
+# 定义 IoU 系数
+def iou_coef(y_true, y_pred, smooth=100):
+    intersection = K.sum(y_true * y_pred)
+    sum = K.sum(y_true + y_pred)
+    iou = (intersection + smooth) / (sum - intersection + smooth)
+    return iou
+
+# 创建数据框
+def create_df(data_dir):
+    images_paths = []
+    masks_paths = glob(f'{data_dir}/*/*_mask*')
+
+    for i in masks_paths:
+        images_paths.append(i.replace('_mask', ''))
+
+    df = pd.DataFrame(data={'images_paths': images_paths, 'masks_paths': masks_paths})
+    return df.sample(n=500, random_state=42)
+
+# 创建数据生成器
+def create_gens(df, aug_dict):
+    img_size = (128, 128)
+    batch_size = 16
+
+    img_gen = ImageDataGenerator(**aug_dict)
+    msk_gen = ImageDataGenerator(**aug_dict)
+
+    image_gen = img_gen.flow_from_dataframe(df, x_col='images_paths', class_mode=None, color_mode='rgb', target_size=img_size,
+                                            batch_size=batch_size, save_to_dir=None, save_prefix='image', seed=1)
+
+    mask_gen = msk_gen.flow_from_dataframe(df, x_col='masks_paths', class_mode=None, color_mode='grayscale', target_size=img_size,
+                                            batch_size=batch_size, save_to_dir=None, save_prefix='mask', seed=1)
+    gen = zip(image_gen, mask_gen)
+
+    for (img, msk) in gen:
+        img = img / 255
+        msk = msk / 255
+        msk[msk > 0.5] = 1
+        msk[msk <= 0.5] = 0
+
+        yield (img, msk)
+
+# 显示图像
+def show_images(images, masks):
+    plt.figure(figsize=(12, 12))
+    for i in range(9):  # 只显示9张图像
+        plt.subplot(3, 3, i+1)
+        img_path = images[i]
+        mask_path = masks[i]
+        # 读取图像并转换为 RGB
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # 读取掩码
+        mask = cv2.imread(mask_path)
+        # 显示图像和掩码
+        plt.imshow(image)
+        plt.imshow(mask, alpha=0.4)
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+# 加载数据集
+data_dir = r"F:\BaiduNetdiskDownload\Brain MRI segmentation\Brain MRI segmentation"
+
+df = create_df(data_dir)
+
+# 分割数据框
+def split_df(df):
+    train_df, dummy_df = train_test_split(df, train_size=0.8)
+    valid_df, test_df = train_test_split(dummy_df, train_size=0.5)
+    return train_df, valid_df, test_df
+
+_, _, test_df = split_df(df)
+
+# 加载模型
+model = load_model(r"F:\cv\CV\unet.h5", custom_objects={'dice_loss': dice_coef, 'iou_coef': iou_coef, 'dice_coef': dice_coef})
+
+# 评估模型
+test_gen = create_gens(test_df, aug_dict={})
+ts_length = len(test_df)
+test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length % n == 0 and ts_length / n <= 80]))
+test_steps = ts_length // test_batch_size
+
+test_score = model.evaluate(test_gen, steps=test_steps, verbose=1)
+
+print("测试损失: ", test_score[0])
+print("测试精度: ", test_score[1])
+print("测试IoU: ", test_score[2])
+print("测试Dice: ", test_score[3])
+
+# 预测并可视化结果
+for _ in range(10):
+    index = np.random.randint(1, len(test_df.index))
+    img = cv2.imread(test_df['images_paths'].iloc[index])
+    img = cv2.resize(img, (128, 128))
+    img = img / 255
+    img = img[np.newaxis, :, :, :]
+
+    predicted_img = model.predict(img)
+
+    plt.figure(figsize=(12, 12))
+    
+    plt.subplot(1, 3, 1)
+    plt.imshow(np.squeeze(img))
+    plt.axis('off')
+    plt.title('原始图像')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(np.squeeze(cv2.imread(test_df['masks_paths'].iloc[index])))
+    plt.axis('off')
+    plt.title('原始掩码')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(np.squeeze(predicted_img) > 0.5)
+    plt.title('预测结果')
+    plt.axis('off')
+    
+    plt.show()
--- backend/unet/unet.py
+++ backend/unet/unet.py
@@ -1,39 +1,26 @@
 import os
-import time
 import random
-import pathlib
-import itertools
 from glob import glob
-from tqdm.notebook import tqdm, tnrange
 from tqdm.keras import TqdmCallback
 
 # 导入数据处理工具
 import cv2
 import numpy as np
 import pandas as pd
-import seaborn as sns
-sns.set_style('darkgrid')
 import matplotlib.pyplot as plt
-from skimage.color import rgb2gray
-#from skimage.morphology import label
-from skimage.transform import resize
 from sklearn.model_selection import train_test_split
-from skimage.io import imread, imshow, concatenate_images
-
-# 导入深度学习库
-import keras
-from keras import backend as K
-from keras.models import Model, load_model, save_model
+from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam, Adamax
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Input, Activation, BatchNormalization, Dropout, Lambda, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate
+from keras.optimizers import Adamax
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Input, Activation, BatchNormalization, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate
+from keras import backend as K
 
 # 忽略警告
 import warnings
 warnings.filterwarnings("ignore")
 
-print('模块加载完成')
+print('Modules loaded')
 
 # 创建数据框
 def create_df(data_dir):
@@ -43,33 +30,28 @@ def create_df(data_dir):
     for i in masks_paths:
         images_paths.append(i.replace('_mask', ''))
 
-    df = pd.DataFrame(data= {'images_paths': images_paths, 'masks_paths': masks_paths})
-
+    df = pd.DataFrame(data={'images_paths': images_paths, 'masks_paths': masks_paths})
     return df
 
 # 分割数据框
 def split_df(df):
-    # 创建训练集数据框
-    train_df, dummy_df = train_test_split(df, train_size= 0.8)
-
-    # 创建验证集和测试集数据框
-    valid_df, test_df = train_test_split(dummy_df, train_size= 0.5)
+    train_df, dummy_df = train_test_split(df, train_size=0.6)
+    valid_df, test_df = train_test_split(dummy_df, train_size=0.5)
     return train_df, valid_df, test_df
 
 # 创建数据生成器
 def create_gens(df, aug_dict):
-    img_size = (256, 256)
-    batch_size = 40
+    img_size = (128, 128)  # 降低图像分辨率
+    batch_size = 16  # 保持批量大小
 
     img_gen = ImageDataGenerator(**aug_dict)
     msk_gen = ImageDataGenerator(**aug_dict)
 
-    # 创建通用生成器
     image_gen = img_gen.flow_from_dataframe(df, x_col='images_paths', class_mode=None, color_mode='rgb', target_size=img_size,
                                             batch_size=batch_size, save_to_dir=None, save_prefix='image', seed=1)
 
     mask_gen = msk_gen.flow_from_dataframe(df, x_col='masks_paths', class_mode=None, color_mode='grayscale', target_size=img_size,
-                                            batch_size=batch_size, save_to_dir=None, save_prefix= 'mask', seed=1)
+                                            batch_size=batch_size, save_to_dir=None, save_prefix='mask', seed=1)
     gen = zip(image_gen, mask_gen)
 
     for (img, msk) in gen:
@@ -80,75 +62,60 @@ def create_gens(df, aug_dict):
 
         yield (img, msk)
 
-# U-Net模型
-def unet(input_size=(256, 256, 3)):
+# 精简的 U-Net 模型
+def unet(input_size=(128, 128, 3)):
     inputs = Input(input_size)
 
-    # 第一层下采样/编码
-    conv1 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(inputs)
+    conv1 = Conv2D(filters=32, kernel_size=(3, 3), padding="same")(inputs)
     bn1 = Activation("relu")(conv1)
-    conv1 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(bn1)
+    conv1 = Conv2D(filters=32, kernel_size=(3, 3), padding="same")(bn1)
     bn1 = BatchNormalization(axis=3)(conv1)
     bn1 = Activation("relu")(bn1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(bn1)
 
-    conv2 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(pool1)
+    conv2 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(pool1)
     bn2 = Activation("relu")(conv2)
-    conv2 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(bn2)
+    conv2 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(bn2)
     bn2 = BatchNormalization(axis=3)(conv2)
     bn2 = Activation("relu")(bn2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(bn2)
 
-    conv3 = Conv2D(filters=256, kernel_size=(3, 3), padding="same")(pool2)
+    conv3 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(pool2)
     bn3 = Activation("relu")(conv3)
-    conv3 = Conv2D(filters=256, kernel_size=(3, 3), padding="same")(bn3)
+    conv3 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(bn3)
     bn3 = BatchNormalization(axis=3)(conv3)
     bn3 = Activation("relu")(bn3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(bn3)
 
-    conv4 = Conv2D(filters=512, kernel_size=(3, 3), padding="same")(pool3)
+    conv4 = Conv2D(filters=256, kernel_size=(3, 3), padding="same")(pool3)
     bn4 = Activation("relu")(conv4)
-    conv4 = Conv2D(filters=512, kernel_size=(3, 3), padding="same")(bn4)
+    conv4 = Conv2D(filters=256, kernel_size=(3, 3), padding="same")(bn4)
     bn4 = BatchNormalization(axis=3)(conv4)
     bn4 = Activation("relu")(bn4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(bn4)
 
-    conv5 = Conv2D(filters=1024, kernel_size=(3, 3), padding="same")(pool4)
+    up5 = concatenate([Conv2DTranspose(128, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn4), conv3], axis=3)
+    conv5 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(up5)
     bn5 = Activation("relu")(conv5)
-    conv5 = Conv2D(filters=1024, kernel_size=(3, 3), padding="same")(bn5)
+    conv5 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(bn5)
     bn5 = BatchNormalization(axis=3)(conv5)
     bn5 = Activation("relu")(bn5)
 
-    up6 = concatenate([Conv2DTranspose(512, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn5), conv4], axis=3)
-    conv6 = Conv2D(filters=512, kernel_size=(3, 3), padding="same")(up6)
+    up6 = concatenate([Conv2DTranspose(64, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn5), conv2], axis=3)
+    conv6 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(up6)
     bn6 = Activation("relu")(conv6)
-    conv6 = Conv2D(filters=512, kernel_size=(3, 3), padding="same")(bn6)
+    conv6 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(bn6)
     bn6 = BatchNormalization(axis=3)(conv6)
     bn6 = Activation("relu")(bn6)
 
-    up7 = concatenate([Conv2DTranspose(256, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn6), conv3], axis=3)
-    conv7 = Conv2D(filters=256, kernel_size=(3, 3), padding="same")(up7)
+    up7 = concatenate([Conv2DTranspose(32, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn6), conv1], axis=3)
+    conv7 = Conv2D(filters=32, kernel_size=(3, 3), padding="same")(up7)
     bn7 = Activation("relu")(conv7)
-    conv7 = Conv2D(filters=256, kernel_size=(3, 3), padding="same")(bn7)
+    conv7 = Conv2D(filters=32, kernel_size=(3, 3), padding="same")(bn7)
     bn7 = BatchNormalization(axis=3)(conv7)
     bn7 = Activation("relu")(bn7)
 
-    up8 = concatenate([Conv2DTranspose(128, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn7), conv2], axis=3)
-    conv8 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(up8)
-    bn8 = Activation("relu")(conv8)
-    conv8 = Conv2D(filters=128, kernel_size=(3, 3), padding="same")(bn8)
-    bn8 = BatchNormalization(axis=3)(conv8)
-    bn8 = Activation("relu")(bn8)
-
-    up9 = concatenate([Conv2DTranspose(64, kernel_size=(2, 2), strides=(2, 2), padding="same")(bn8), conv1], axis=3)
-    conv9 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(up9)
-    bn9 = Activation("relu")(conv9)
-    conv9 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(bn9)
-    bn9 = BatchNormalization(axis=3)(conv9)
-    bn9 = Activation("relu")(bn9)
-
-    conv10 = Conv2D(filters=1, kernel_size=(1, 1), activation="sigmoid")(bn9)
-    return Model(inputs=[inputs], outputs=[conv10])
+    conv8 = Conv2D(filters=1, kernel_size=(1, 1), activation="sigmoid")(bn7)
+    return Model(inputs=[inputs], outputs=[conv8])
 
 # Dice 系数
 def dice_coef(y_true, y_pred, smooth=100):
@@ -173,8 +140,8 @@ def iou_coef(y_true, y_pred, smooth=100):
 # 显示图像
 def show_images(images, masks):
     plt.figure(figsize=(12, 12))
-    for i in range(25):
-        plt.subplot(5, 5, i+1)
+    for i in range(9):  # 只显示9张图像
+        plt.subplot(3, 3, i+1)
         img_path = images[i]
         mask_path = masks[i]
         # 读取图像并转换为 RGB
@@ -192,11 +159,6 @@ def show_images(images, masks):
 
 # 绘制训练过程
 def plot_training(hist):
-    '''
-    该函数接收训练模型，并绘制精度和损失的历史记录，标记出最佳的epoch。
-    '''
-
-    # 定义所需变量
     tr_acc = hist.history['accuracy']
     tr_iou = hist.history['iou_coef']
     tr_dice = hist.history['dice_coef']
@@ -217,51 +179,47 @@ def plot_training(hist):
 
     Epochs = [i+1 for i in range(len(tr_acc))]
 
-    acc_label = f'最佳 epoch= {str(index_acc + 1)}'
-    iou_label = f'最佳 epoch= {str(index_iou + 1)}'
-    dice_label = f'最佳 epoch= {str(index_dice + 1)}'
-    loss_label = f'最佳 epoch= {str(index_loss + 1)}'
-    # 绘制训练历史记录
-    plt.figure(figsize= (20, 20))
+    acc_label = f'Best epoch= {str(index_acc + 1)}'
+    iou_label = f'Best epoch= {str(index_iou + 1)}'
+    dice_label = f'Best epoch= {str(index_dice + 1)}'
+    loss_label = f'Best epoch= {str(index_loss + 1)}'
+    plt.figure(figsize=(20, 20))
     plt.style.use('fivethirtyeight')
 
-    # 训练精度
     plt.subplot(2, 2, 1)
-    plt.plot(Epochs, tr_acc, 'r', label= '训练精度')
-    plt.plot(Epochs, val_acc, 'g', label= '验证精度')
-    plt.scatter(index_acc + 1 , acc_highest, s= 150, c= 'blue', label= acc_label)
-    plt.title('训练和验证精度')
+    plt.plot(Epochs, tr_acc, 'r', label='Training Accuracy')
+    plt.plot(Epochs, val_acc, 'g', label='Validation Accuracy')
+    plt.scatter(index_acc + 1 , acc_highest, s=150, c='blue', label=acc_label)
+    plt.title('Training and Validation Accuracy')
     plt.xlabel('Epochs')
-    plt.ylabel('精度')
+    plt.ylabel('Accuracy')
     plt.legend()
-    # 训练IoU
+
     plt.subplot(2, 2, 2)
-    plt.plot(Epochs, tr_iou, 'r', label= '训练IoU')
-    plt.plot(Epochs, val_iou, 'g', label= '验证IoU')
-    plt.scatter(index_iou + 1 , iou_highest, s= 150, c= 'blue', label= iou_label)
-    plt.title('训练和验证IoU系数')
+    plt.plot(Epochs, tr_iou, 'r', label='Training IoU')
+    plt.plot(Epochs, val_iou, 'g', label='Validation IoU')
+    plt.scatter(index_iou + 1 , iou_highest, s=150, c='blue', label=iou_label)
+    plt.title('Training and Validation IoU Coefficient')
     plt.xlabel('Epochs')
     plt.ylabel('IoU')
     plt.legend()
 
-    # 训练Dice
     plt.subplot(2, 2, 3)
-    plt.plot(Epochs, tr_dice, 'r', label= '训练Dice')
-    plt.plot(Epochs, val_dice, 'g', label= '验证Dice')
-    plt.scatter(index_dice + 1 , dice_highest, s= 150, c= 'blue', label= dice_label)
-    plt.title('训练和验证Dice系数')
+    plt.plot(Epochs, tr_dice, 'r', label='Training Dice')
+    plt.plot(Epochs, val_dice, 'g', label='Validation Dice')
+    plt.scatter(index_dice + 1 , dice_highest, s=150, c='blue', label=dice_label)
+    plt.title('Training and Validation Dice Coefficient')
     plt.xlabel('Epochs')
     plt.ylabel('Dice')
     plt.legend()
 
-    # 训练损失
     plt.subplot(2, 2, 4)
-    plt.plot(Epochs, tr_loss, 'r', label= '训练损失')
-    plt.plot(Epochs, val_loss, 'g', label= '验证损失')
-    plt.scatter(index_loss + 1, val_lowest, s= 150, c= 'blue', label= loss_label)
-    plt.title('训练和验证损失')
+    plt.plot(Epochs, tr_loss, 'r', label='Training Loss')
+    plt.plot(Epochs, val_loss, 'g', label='Validation Loss')
+    plt.scatter(index_loss + 1, val_lowest, s=150, c='blue', label=loss_label)
+    plt.title('Training and Validation Loss')
     plt.xlabel('Epochs')
-    plt.ylabel('损失')
+    plt.ylabel('Loss')
     plt.legend()
 
     plt.tight_layout()
@@ -287,13 +245,13 @@ test_gen = create_gens(test_df, aug_dict={})
 show_images(list(train_df['images_paths']), list(train_df['masks_paths']))
 
 model = unet()
-model.compile(Adamax(learning_rate= 0.001), loss= dice_loss, metrics= ['accuracy', iou_coef, dice_coef])
+model.compile(Adamax(learning_rate=0.001), loss=dice_loss, metrics=['accuracy', iou_coef, dice_coef])
 
 model.summary()
 
-epochs = 1
-batch_size = 1
-callbacks = [ModelCheckpoint('unet.hdf5', verbose=0, save_best_only=True), TqdmCallback()]
+epochs = 30  # 调整训练轮数
+batch_size = 16  # 保持批量大小
+callbacks = [ModelCheckpoint('unet.h5', verbose=0, save_best_only=True), TqdmCallback()]
 
 history = model.fit(train_gen,
                     steps_per_epoch=len(train_df) / batch_size,
@@ -313,27 +271,27 @@ train_score = model.evaluate(train_gen, steps=test_steps, verbose=1)
 valid_score = model.evaluate(valid_gen, steps=test_steps, verbose=1)
 test_score = model.evaluate(test_gen, steps=test_steps, verbose=1)
 
-print("训练损失: ", train_score[0])
-print("训练精度: ", train_score[1])
-print("训练IoU: ", train_score[2])
-print("训练Dice: ", train_score[3])
+print("Train Loss: ", train_score[0])
+print("Train Accuracy: ", train_score[1])
+print("Train IoU: ", train_score[2])
+print("Train Dice: ", train_score[3])
 print('-' * 20)
 
-print("验证损失: ", valid_score[0])
-print("验证精度: ", valid_score[1])
-print("验证IoU: ", valid_score[2])
-print("验证Dice: ", valid_score[3])
+print("Validation Loss: ", valid_score[0])
+print("Validation Accuracy: ", valid_score[1])
+print("Validation IoU: ", valid_score[2])
+print("Validation Dice: ", valid_score[3])
 print('-' * 20)
 
-print("测试损失: ", test_score[0])
-print("测试精度: ", test_score[1])
-print("测试IoU: ", test_score[2])
-print("测试Dice: ", test_score[3])
+print("Test Loss: ", test_score[0])
+print("Test Accuracy: ", test_score[1])
+print("Test IoU: ", test_score[2])
+print("Test Dice: ", test_score[3])
 
-for _ in range(20):
+for _ in range(10):  # 减少预测显示的图像数量
     index = np.random.randint(1, len(test_df.index))
     img = cv2.imread(test_df['images_paths'].iloc[index])
-    img = cv2.resize(img, (256, 256))
+    img = cv2.resize(img, (128, 128))
     img = img / 255
     img = img[np.newaxis, :, :, :]
 
@@ -344,16 +302,16 @@ for _ in range(20):
     plt.subplot(1, 3, 1)
     plt.imshow(np.squeeze(img))
     plt.axis('off')
-    plt.title('原始图像')
+    plt.title('Original Image')
 
     plt.subplot(1, 3, 2)
     plt.imshow(np.squeeze(cv2.imread(test_df['masks_paths'].iloc[index])))
     plt.axis('off')
-    plt.title('原始掩码')
+    plt.title('Original Mask')
 
     plt.subplot(1, 3, 3)
     plt.imshow(np.squeeze(predicted_img) > 0.5)
-    plt.title('预测结果')
+    plt.title('Prediction')
     plt.axis('off')
     
     plt.show()
